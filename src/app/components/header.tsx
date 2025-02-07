48e8e0280e1ea70c7516148757ec583957a3e019
Paul Chukwuemeka <paulchukwuemeka123@gmail.com>
2025-02-07 13:55:06 +0100

    Refactor Header component styles to remove redundant border class; improve consistency across archive and main headers

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -77,7 +77,7 @@ const Header = () => {
   return (
     <div
       className={
-        "border border-t-0  w-full relative p-4 px-10 items-center flex justify-between max-h-[13vh] col-span-full"
+        "border w-full relative p-4 px-10 items-center flex justify-between max-h-[13vh] col-span-full"
       }
     >
       <h1 className="font-bold text-2xl">
